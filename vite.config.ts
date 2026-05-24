99e6a5285353d58e60bfddc1b14d3cd4ef10e49d
seraprogrammer <programmernazmul1@gmail.com>
2026-05-24 21:43:21 +0600

    fix: set vite base path to relative for electron

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: './',
   plugins: [solid(), tailwindcss()],
   build: {
     outDir: 'dist-renderer',
