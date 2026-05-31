ae57ba98fa7a422d16805d7c95ec75915cec13a5
seraprogrammer <programmernazmul1@gmail.com>
2026-05-31 13:07:02 +0600

    refactor: make boot splash draggable and update copy icon to premium filled SVG

--- packages/ui/src/components/markdown.tsx
+++ packages/ui/src/components/markdown.tsx
@@ -36,7 +36,7 @@ const config = {
 }
 
 const iconPaths = {
-  copy: '<path d="M6.2513 6.24935V2.91602H17.0846V13.7493H13.7513M13.7513 6.24935V17.0827H2.91797V6.24935H13.7513Z" stroke="currentColor" stroke-linecap="round"/>',
+  copy: '<path fill-rule="evenodd" clip-rule="evenodd" d="M1 9.50006C1 10.3285 1.67157 11.0001 2.5 11.0001H4L4 10.0001H2.5C2.22386 10.0001 2 9.7762 2 9.50006L2 2.50006C2 2.22392 2.22386 2.00006 2.5 2.00006L9.5 2.00006C9.77614 2.00006 10 2.22392 10 2.50006V4.00002H5.5C4.67158 4.00002 4 4.67159 4 5.50002V12.5C4 13.3284 4.67158 14 5.5 14H12.5C13.3284 14 14 13.3284 14 12.5V5.50002C14 4.67159 13.3284 4.00002 12.5 4.00002H11V2.50006C11 1.67163 10.3284 1.00006 9.5 1.00006H2.5C1.67157 1.00006 1 1.67163 1 2.50006V9.50006ZM5 5.50002C5 5.22388 5.22386 5.00002 5.5 5.00002H12.5C12.7761 5.00002 13 5.22388 13 5.50002V12.5C13 12.7762 12.7761 13 12.5 13H5.5C5.22386 13 5 12.7762 5 12.5V5.50002Z" fill="currentColor"/>',
   check: '<path d="M5 11.9657L8.37838 14.7529L15 5.83398" stroke="currentColor" stroke-linecap="square"/>',
 }
 
@@ -84,7 +84,7 @@ function createIcon(path: string, slot: string) {
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
   svg.setAttribute("data-slot", "icon-svg")
   svg.setAttribute("fill", "none")
-  svg.setAttribute("viewBox", "0 0 20 20")
+  svg.setAttribute("viewBox", slot === "copy-icon" ? "0 0 15 15" : "0 0 20 20")
   svg.setAttribute("aria-hidden", "true")
   svg.innerHTML = path
   icon.appendChild(svg)
